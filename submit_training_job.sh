defe6a02a1814c7eec67a1b16a19d1dbf6acf244
CasualMathEnjoyer <morovicsova.katka@gmail.com>
2025-12-15 17:19:22 +0100

    submit training job

---
+++ submit_training_job.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#PBS -N hiero_transformer_training
+#PBS -l walltime=24:00:00
+#PBS -q gpu
+#PBS -j oe
+#PBS -l select=1:mem=32G:ncpus=8:ngpus=1
+
+cd $PBS_O_WORKDIR
+
+module load cuda/12.4.1
+
+source /mnt/lustre/helios-home/morovkat/miniconda3/etc/profile.d/conda.sh
+conda activate hiero-transformer
+
+cd /mnt/lustre/helios-home/morovkat/hiero-transformer_fork
+
+python train_minimal.py
+
